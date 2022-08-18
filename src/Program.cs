fa6569e6db301972a4fceff3b98d74de8c0eb241
graham-phillips <graham.phillips@paddypowerbetfair.com>
2022-08-18 17:39:25 +0100

    docker build

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+using KafkaIntgrationTestsInGithubActions;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
