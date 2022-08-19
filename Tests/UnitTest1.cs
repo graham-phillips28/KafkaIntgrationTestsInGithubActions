bc563f6aff98eafb157739915d6ae716afb23fa8
graham-phillips <graham.phillips@paddypowerbetfair.com>
2022-08-19 09:37:17 +0100

    Add docker compose

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Confluent.Kafka;
+using System.Net;
+using System.Threading;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1Async()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "127.0.0.1:9092",
+            };
+
+            var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            var response = await producer.ProduceAsync("test", new Message<Null, string> { Value = "message" });
+        }
+
+        [Fact]
+        public async Task Test2Async()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "127.0.0.1:9092",
+                GroupId = "foo",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+            consumer.Subscribe("test");
+
+            //var consumeResult = consumer.Consume(new CancellationToken());
+            consumer.Close();
+        }
+    }
+}
