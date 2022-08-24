9b84856847273cf70c3cbb544cb4619a9ae068de
graham-phillips <graham.phillips@paddypowerbetfair.com>
2022-08-24 11:59:33 +0100

    Clean up test

--- Tests/KafkaTests.cs
+++ Tests/KafkaTests.cs
@@ -9,13 +9,35 @@ namespace Tests
     {
 
         [Fact]
-        public async Task Test1Async()
+        public async Task ProduceToTopicThenConsume()
         {
+            //Arrange
             var testMessage = "test message";
+            var consumer = GetTestConsumer();
+            var producer = GetTestProducer();
+
+            //Act
+            await producer.ProduceAsync("test-topic", new Message<Null, string> { Value = testMessage });
+            consumer.Subscribe("test-topic");
+            var consumeResult = consumer.Consume(new CancellationToken());
+            consumer.Close();
+
+            //Assert
+            Assert.Equal(testMessage, consumeResult.Message.Value);
+        }
+
+        public IProducer<Null, string> GetTestProducer()
+        {
             var produceConfig = new ProducerConfig
             {
                 BootstrapServers = "kafka1:19092",
             };
+
+            return new ProducerBuilder<Null, string>(produceConfig).Build();
+        }
+
+        public IConsumer<Null, string> GetTestConsumer()
+        {
             var consumeConfig = new ConsumerConfig
             {
                 BootstrapServers = "kafka1:19092",
@@ -23,21 +45,7 @@ namespace Tests
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
-            var consumer = new ConsumerBuilder<Null, string>(consumeConfig).Build();
-
-            
-            
-            var producer = new ProducerBuilder<Null, string>(produceConfig).Build();
-            Console.WriteLine("Console working");
-            var response = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value = testMessage });
-            Console.WriteLine(response.ToString());
-
-            consumer.Subscribe("test-topic");
-            var consumeResult = consumer.Consume(new CancellationToken());
-            Console.WriteLine(consumeResult.Message.Value);
-            Assert.Equal(testMessage, consumeResult.Message.Value);
-            consumer.Close();
-
+            return new ConsumerBuilder<Null, string>(consumeConfig).Build();
         }
     }
 }
