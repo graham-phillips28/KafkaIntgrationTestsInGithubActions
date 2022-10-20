a2f7de8a30a32bc4eecbe5bf76bea6ee0921bb0d
graham-phillips <graham.phillips@paddypowerbetfair.com>
2022-10-20 09:23:41 +0100

    Remove usings

--- src/Application/StringHandler.cs
+++ src/Application/StringHandler.cs
@@ -1,11 +1,6 @@
 ﻿using Confluent.Kafka;
 using KafkaIntgrationTestsInGithubActions.Application.Interfaces;
 using KafkaIntgrationTestsInGithubActions.Kafka.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KafkaIntgrationTestsInGithubActions.Application
 {
